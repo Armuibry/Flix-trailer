e41cb1d7e6a204f7ac6064edff9cf204358cd932
Md Amir Sohail <mdamirsohail1@gmail.com>
2022-12-04 17:12:50 +0530

    flix tv Trailer app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MoviePage from './movies/MoviePage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MoviePage />
+    </div>
+  );
+}
+
+export default App;
